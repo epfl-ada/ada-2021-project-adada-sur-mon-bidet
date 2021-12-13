f10f06aafb4edb0f00cd9553c1bc4d93cf8148e2
Lucas Trognon <lucas.trognon@arcanite.ch>
2021-12-13 23:47:58 +0100

    sync avant dodo : debut de template

--- web/adada.py
+++ web/adada.py
@@ -4,3 +4,12 @@ app = Flask(__name__)
 @app.route("/")
 def index():
      return render_template("index.html")
+
+@app.route("/bonus", methods=["GET","POST"])
+def bonus():
+
+
+
+
+
+    return render_template("bonus.html", original="test1", found = "test2")
