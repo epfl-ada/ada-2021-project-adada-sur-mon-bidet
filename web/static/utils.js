48f616f8254b0cfb11c0c84a396657890cf97239
Lucas Trognon <lucas.trognon@arcanite.ch>
2021-12-14 14:31:24 +0100

    progress bar

---
+++ web/static/utils.js
@@ -0,0 +1,7 @@
+
+function updateProgress() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("progressbar").style.width = scrolled + "%";
+}
