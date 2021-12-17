3a825880ccc4788debe70616c5af8ea6c53608c9
Lucas Trognon <lucas.trognon@arcanite.ch>
2021-12-17 11:39:01 +0100

    fixed nav

--- web/static/utils.js
+++ web/static/utils.js
@@ -5,3 +5,18 @@ function updateProgress() {
   var scrolled = (winScroll / height) * 100;
   document.getElementById("progressbar").style.width = scrolled + "%";
 }
+
+function scrollToPart(id){
+  var node = document.getElementById(id);
+  var yourHeight = 72;
+
+  // scroll to your element
+ // node.scrollIntoView(true);
+  window.scroll(0, node.offsetTop - yourHeight);
+  // now account for fixed header
+  //var scrolledY = window.scrollY;
+
+  //if(scrolledY){
+  //  window.scroll(0, scrolledY - yourHeight);
+  //}
+}
